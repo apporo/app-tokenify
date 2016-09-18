93ac23774e2a0adc99a197f27a9a2d8d304f8b90
pnhung177 <pnhung177@acegik.net>
2016-09-19 06:13:42 +0700

    Implements the ConfigUserStore and FileUserStore

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
     appTokenify: {
+      expiresIn: 86400,
+      ignoreExpiration: false,
       passphrase: 'sup3rs3cr3tp4ssw0rd',
       protectedPaths: []
     }
