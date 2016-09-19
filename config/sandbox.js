8dc4988e3bc69b681bee2f1bf2b04703b29fc2fa
pnhung177 <pnhung177@acegik.net>
2016-09-19 10:53:43 +0700

    Refactoring the UserStore -> EntrypointStore

--- config/sandbox.js
+++ config/sandbox.js
@@ -1,9 +1,13 @@
 module.exports = {
   plugins: {
     appTokenify: {
-      expiresIn: 86400,
-      ignoreExpiration: false,
-      passphrase: 'sup3rs3cr3tp4ssw0rd',
+      jwt: {
+        tokenHeaderName: 'x-access-token',
+        tokenQueryName: 'token',
+        expiresIn: 86400,
+        ignoreExpiration: false,
+        password: 'sup3rs3cr3tp4ssw0rd'
+      },
       protectedPaths: []
     }
   }
