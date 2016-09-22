894387c0711427e1b6eaadcd8699d0927efb9374
pnhung177 <pnhung177@acegik.net>
2016-09-23 01:52:34 +0700

    Implements the restEntrypointStore; apply the credential include 3 fields: scope, key, secret; save authentication and permissions into req.sessionJWT (session token object)

--- config/sandbox.js
+++ config/sandbox.js
@@ -5,11 +5,36 @@ module.exports = {
       jwt: {
         tokenHeaderName: 'x-access-token',
         tokenQueryName: 'token',
+        tokenObjectName: 'sessionJWT',
         expiresIn: 86400,
         ignoreExpiration: false,
-        password: 'sup3rs3cr3tp4ssw0rd'
+        secretkey: 'sup3rs3cr3tp4ssw0rd'
       },
-      protectedPaths: []
+      fieldNameRef: {
+        scope: 'realm',
+        key: 'key',
+        secret: 'secret'
+      },
+      protectedPaths: [],
+      entrypointStore: {
+        entrypoints: [
+          {
+            key: 'master',
+            secret: '$2a$10$L9t1sDIP4u2xcKGnte8D7uWhzPDRUTPSKD1AFWVEw833/cqp0gyPC'
+          }
+        ]
+      },
+      entrypointStoreFile: require('path').join(__dirname, '../data/entrypointstore.json'),
+      entrypointStoreRest: {
+        sources: [
+          {
+            enabled: false,
+            url: 'http://localhost:3000/auth',
+            authType: 'none',
+            transform: function(response) { return response; }
+          }
+        ]
+      }
     }
   }
 };
