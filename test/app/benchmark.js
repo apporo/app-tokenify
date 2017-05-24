50e30a0a663ae2bf1080e112e433a24663973620
pnhung177 <pnhung177@acegik.net>
2017-05-24 17:11:07 +0700

    Add more benchmarks

--- test/app/benchmark.js
+++ test/app/benchmark.js
@@ -12,71 +12,83 @@ var Benchmark = require('benchmark');
 
 var suite = new Benchmark.Suite('mix', {});
 
-suite.add('httpauth-authenticated', {
-	'defer': true,
-	'fn': function(deferred) {
-		request({
-			method: 'GET',
-			url: 'http://localhost:7979/tokenify/httpauth/authorized',
-			json: true,
-			auth: {
-				username: 'static1',
-				password: 'dobietday',
-				sendImmediately: false
-			}
-		}, function(err, res, body) {
-			debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
-			deferred.resolve();
-		});
-	}
-});
+var checkSkip = function(name) {
+	return (!process.env.BENCHMARK || process.env.BENCHMARK.indexOf(name) >= 0);
+}
 
-suite.add('httpauth-public', {
-	'defer': true,
-	'fn': function(deferred) {
-		request({
-			method: 'GET',
-			url: 'http://localhost:7979/tokenify/httpauth/public',
-			json: true
-		}, function(err, res, body) {
-			debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
-			deferred.resolve();
-		});
-	}
-});
+if (checkSkip('11')) {
+	suite.add('11-httpauth-authenticated', {
+		'defer': true,
+		'fn': function(deferred) {
+			request({
+				method: 'GET',
+				url: 'http://localhost:7979/tokenify/httpauth/authorized',
+				json: true,
+				auth: {
+					username: 'static1',
+					password: 'dobietday',
+					sendImmediately: false
+				}
+			}, function(err, res, body) {
+				debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
+				deferred.resolve();
+			});
+		}
+	});
+}
 
-suite.add('mix-authenticated', {
-	'defer': true,
-	'fn': function(deferred) {
-		request({
-			method: 'GET',
-			url: 'http://localhost:7979/tokenify/mix1/authorized',
-			json: true,
-			auth: {
-				username: 'operator',
-				password: 'dobietday',
-				sendImmediately: false
-			}
-		}, function(err, res, body) {
-			debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
-			deferred.resolve();
-		});
-	}
-});
+if (checkSkip('12')) {
+	suite.add('12-httpauth-public', {
+		'defer': true,
+		'fn': function(deferred) {
+			request({
+				method: 'GET',
+				url: 'http://localhost:7979/tokenify/httpauth/public',
+				json: true
+			}, function(err, res, body) {
+				debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
+				deferred.resolve();
+			});
+		}
+	});
+}
 
-suite.add('mix-public', {
-	'defer': true,
-	'fn': function(deferred) {
-		request({
-			method: 'GET',
-			url: 'http://localhost:7979/tokenify/mix2/authorized',
-			json: true
-		}, function(err, res, body) {
-			debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
-			deferred.resolve();
-		});
-	}
-});
+if (checkSkip('31')) {
+	suite.add('31-mix-authenticated', {
+		'defer': true,
+		'fn': function(deferred) {
+			request({
+				method: 'GET',
+				url: 'http://localhost:7979/tokenify/mix1/authorized',
+				json: true,
+				auth: {
+					username: 'operator',
+					password: 'dobietday',
+					sendImmediately: false
+				}
+			}, function(err, res, body) {
+				debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
+				deferred.resolve();
+			});
+		}
+	});
+}
+
+if (checkSkip('32')) {
+	suite.add('32-mix-public', {
+		'defer': true,
+		'fn': function(deferred) {
+			request({
+				method: 'GET',
+				url: 'http://localhost:7979/tokenify/mix2/authorized',
+				json: true
+			}, function(err, res, body) {
+				debuglog.isEnabled && debuglog('statusCode: %s; body: %s', res.statusCode, JSON.stringify(body));
+				deferred.resolve();
+			});
+		}
+	});
+}
 
 suite.on('cycle', function(event) {
 	console.log(String(event.target));
